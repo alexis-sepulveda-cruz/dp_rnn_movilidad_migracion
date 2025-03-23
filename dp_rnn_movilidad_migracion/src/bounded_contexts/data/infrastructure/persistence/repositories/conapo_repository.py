f5ea8f995c3e09cbf36ca170c2edc5b34fb07452
Armando Alexis Sepúlveda Cruz <armando.sepulvedacrz@gmail.com>
2025-03-22 22:16:08 -0600

    Eliminar importación no utilizada de LoggerPort en conapo_repository.py

--- dp_rnn_movilidad_migracion/src/bounded_contexts/data/infrastructure/persistence/repositories/conapo_repository.py
+++ dp_rnn_movilidad_migracion/src/bounded_contexts/data/infrastructure/persistence/repositories/conapo_repository.py
@@ -6,7 +6,6 @@ from pathlib import Path
 from typing import Dict, Any, List
 import pandas as pd
 
-from dp_rnn_movilidad_migracion.src.shared.domain.ports.logger_port import LoggerPort
 from dp_rnn_movilidad_migracion.src.bounded_contexts.data.domain.ports.data_repository import DataRepository
 from dp_rnn_movilidad_migracion.src.bounded_contexts.data.infrastructure.persistence.schemas.conapo_schema import (
     TEMPORAL_FEATURES,
